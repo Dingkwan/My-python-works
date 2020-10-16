8708f5523a332aead6cfa027d5a1e4a7c53075cb
Dingkwan <72882556+Dingkwan@users.noreply.github.com>
2020-10-16 19:39:42 +0800

    Update p1.py

--- 實驗/3/p1.py
+++ 實驗/3/p1.py
@@ -1,8 +1,5 @@
 def letters_in_string(string):
-    count=0
-    for letter in string:
-        count+=1
-    return count
+    return len(str(string))
 
 String=input('请输入字串：')
 print('这个字串有',letters_in_string(String),'个字符')
